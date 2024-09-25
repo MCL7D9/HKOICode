e1200a709398755ee32f03de3dd45e2730318f0d
MCL7D9 <101371148+MCL7D9@users.noreply.github.com>
2024-09-25 01:46:43 +0000

    general

--- TextGraphics.c
+++ TextGraphics.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(){
      int x=0,a=0;
      scanf("%d",&x);
-     for (int i=0;i<floor(x/2);i++){
+     for (int i=0;i<x;i++){
           a=(int)floor(x/2)-i;
-          for (int j=0;j<x;j++){
-               if (i==a||i==(x-a)) {
-                    printf("#");
-               } else {
-                    printf("-");
-               }
+          for (int j=0;j<(x+1);j++){  
+              if (j==(abs(a))||j==(x-a)||j==(x+a)) {
+                 printf("#");
+              } else {
+                printf("-");
+              }
           }
           printf("\n");
      }
-
+     system("pause");
      return 0;
 }
